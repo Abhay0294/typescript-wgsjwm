12be1d91062beb18f2312a29d4d0bb6f54a7998d
Abhay0294 <abhaypratap349@gmail.com>
2022-06-17 13:40:56 +0530

    MOdifications done to support multiple elements at once

--- index.ts
+++ index.ts
@@ -3,6 +3,16 @@ import './style.css';
 
 const domElement = document.getElementById('app');
 const childElement = document.createElement('div');
+const childElement2 = document.createElement('div');
 childElement.className = 'childDiv';
-childElement.innerHTML = '<button>Click Me</Button>';
-domElement.appendChild(childElement);
+childElement2.className = 'childDiv1';
+childElement.innerHTML = '<button class="button">Click Me</Button>';
+childElement2.innerHTML = '<button>Click Me</Button>';
+domElement.append(childElement, childElement2);
+
+/**
+ * Use append() method to append multiple elements to the
+ * parent node at once.
+ * Use appendChild() method to append only one child node to
+ * the parent
+ */
